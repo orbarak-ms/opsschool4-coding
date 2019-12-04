90afb5340fc6c14b50f2f4c85fd09778e85fca1e
orbarak-ms <46415241+orbarak-ms@users.noreply.github.com>
2019-12-04 13:53:34 +0200

    Update hw.py

--- hw.py
+++ hw.py
@@ -4,17 +4,14 @@ if __name__ == '__main__':
     with open('hw.json') as import_file:
         data = json.load(import_file)
 
-    sorted_ages = sorted(data['buckets'])
+    sorted_ages = data['buckets']
     people_list = data['ppl_ages']
     extra_age = [0, max(people_list.values()) + 1]
     sorted_ages.extend(extra_age)
     sorted_ages = sorted(sorted_ages)
 
     for i in range(len(sorted_ages) - 1):
-        print(sorted_ages[i], end="")
-        print("-", end="")
-        print(sorted_ages[i + 1], end="")
-        print(":")
+        print(sorted_ages[i], "-", sorted_ages[i + 1], ":", sep="")
         for key in people_list:
             if people_list[key] >= sorted_ages[i]:
                 if people_list[key] < sorted_ages[i + 1]:
