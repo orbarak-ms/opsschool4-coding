52197c3a45cf0b0c538fee88f8b4ef3060ad1697
orbarak-ms <46415241+orbarak-ms@users.noreply.github.com>
2019-12-04 13:50:01 +0200

    made code nicer

--- hw.py
+++ hw.py
@@ -6,17 +6,16 @@ if __name__ == '__main__':
 
     sorted_ages = sorted(data['buckets'])
     people_list = data['ppl_ages']
-    extra_age = []
-    extra_age.append(0)
-    extra_age.append(max(people_list.values()) + 1)
+    extra_age = [0, max(people_list.values()) + 1]
     sorted_ages.extend(extra_age)
     sorted_ages = sorted(sorted_ages)
 
-    for i in range(len(sorted_ages)-1):
-        print(sorted_ages[i],end="")
-        print("-",end="")
-        print(sorted_ages[i+1],end="")
+    for i in range(len(sorted_ages) - 1):
+        print(sorted_ages[i], end="")
+        print("-", end="")
+        print(sorted_ages[i + 1], end="")
         print(":")
         for key in people_list:
-            if people_list[key] >= sorted_ages[i] and people_list[key] < sorted_ages[i+1]:
-                print("  - ", key)
+            if people_list[key] >= sorted_ages[i]:
+                if people_list[key] < sorted_ages[i + 1]:
+                    print("  -", key)
